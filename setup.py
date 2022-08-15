a29b74e3ac2c9834b99023cfcf813ae5e65ff59b
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2022-08-15 08:20:05 +0200

    Bumped version to `1.0.3`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PTP",
-    version="1.0.2",
+    version="1.0.3",
     description="pygame to pillow",
     author="andrew",
     project_urls={
